868e582c4d60635d57e3f27b6227b9543c37e43f
Xurlz <charlessena2010@bol.com.br>
2020-10-24 10:56:28 -0300

    Reestruturação da comunicação com o banco em relação ao cadastro

--- laravel/app/Http/Controllers/CadastroClienteController.php
+++ laravel/app/Http/Controllers/CadastroClienteController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\Usuario;
 
 class CadastroClienteController extends Controller
 {
@@ -15,39 +15,15 @@ class CadastroClienteController extends Controller
 
     public function store(Request $formRequest)
     {
-        // Banco está inconsistente, considerar refazê-lo. Palavras estão em case sensitive.
-        // Banco de dados não condiz com o formulário
-        //Substituir por um ORM
-        DB::insert(
-            "INSERT INTO cliente (
-                Nome,
-                -- data_nascimento, -- Não usado
-                celular,
-                email,
-                -- lat, -- Não usado
-                -- lng, -- Não usado
-                senha
-            ) 
-            VALUES (
-                :nome,
-                -- :data_nascimento,
-                :celular,
-                :email,
-                -- :lat,
-                -- :lng, 
-                :senha
-            );",
-            // Colocar em array isolado
-            [
-                ":nome" => $formRequest->nome_cliente,
-                // ":data_nascimento" => $formRequest->data_nascimento, // Não usado
-                ":celular" => $formRequest->tel_cliente,
-                ":email" => $formRequest->email_cliente,
-                // ":lat" => $formRequest->lat, // Não usado
-                // ":lng" => $formRequest->lng, // Não usado
-                ":senha" => sha1($formRequest->senha)
-            ]
-        );
+        $email = $formRequest->email_cliente;
+        $senha = $formRequest->senha_cliente;
+        
+        $usuario = new Usuario();
+        $usuario->email = $email;
+        $usuario->senha = $senha;
+
+
+        var_dump($usuario->save());
 
         return redirect('/');
        
