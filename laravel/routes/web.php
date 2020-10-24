c9ad6372b9ea86254b0c15ff815f81f01b2bd30e
Xurlz <charlessena2010@bol.com.br>
2020-10-24 08:25:19 -0300

    Trocando rota para a pagina inicial e removendo rota padrao do laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/teste', function(){ return view('teste.index');});
 
-Route::get('/home', function(){ return view('home.index');});
+Route::get('/', function(){ return view('home.index');});
