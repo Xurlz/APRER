7eaadc8ec76971951f27fd9bead64776d42e0a3a
Xurlz <charlessena2010@bol.com.br>
2020-10-30 02:06:17 -0300

    Compilação e importação de assets. Jquery e bootstrap

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('jquery/jquery.js') }}" defer></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +20,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
