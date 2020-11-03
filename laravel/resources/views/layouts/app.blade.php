5666e28c3bb797e599ba8eaa7177d3b763337a51
Xurlz <charlessena2010@bol.com.br>
2020-11-03 20:01:10 -0300

    Remoção de fonte

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -14,10 +14,6 @@
     <script src="{{ asset('jquery/jquery.js') }}" defer></script>
     <script src="{{ asset('bootstrap/js/bootstrap.bundle.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
