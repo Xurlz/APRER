ef6ee3a4335dd1cb5f7d140a5fc8d155b303a995
Xurlz <charlessena2010@bol.com.br>
2020-11-04 13:04:57 -0300

    Implementação do mapa na tela inicial

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -1,3 +1,6 @@
+@php
+    $paginaInicial = url()->current() === route('pagina_inicial');
+@endphp
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -21,6 +24,15 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    @if($paginaInicial)
+    <style>
+        #map {
+            height: 800px;
+            width: 100%;
+        }
+    </style>
+    @endif
+    
 </head>
 <body>
     <div id="app">
@@ -74,10 +86,92 @@
                 </div>
             </div>
         </nav>
+        @if ($paginaInicial)
+            <div id="map"></div>
+            <script>              
+                var map, infoWindow;
 
+                function initMap() {
+                    console.log(document.getElementById('map'))
+                    map = new google.maps.Map(document.getElementById('map'), 
+                    {
+                        center: {lat: -23.556583, lng: -46.658813},
+                        zoom: 15
+                    });
+
+                    var icons = {
+                        profissional: {
+                        icon: "{{asset('img/worker_30px.png')}}"
+                        },
+                        cliente: {
+                        icon: "{{asset('img/hand_dollar.png')}}"
+                        }
+                    };
+
+                    var features = [
+                        {position: new google.maps.LatLng(-23.559203, -46.658943), type: 'cliente'},                 
+                        {position: new google.maps.LatLng(-23.550887, -46.662143), type: 'profissional'},                 
+                        {position: new google.maps.LatLng(-23.561184, -46.661664), type: 'cliente'},                 
+                        {position: new google.maps.LatLng(-23.555060, -46.648190), type: 'profissional'}, 
+                        {position: new google.maps.LatLng(-23.558428, -46.665875), type: 'cliente'},                
+                        {position: new google.maps.LatLng(-23.562325, -46.659347), type: 'profissional'},  
+                        {position: new google.maps.LatLng(-23.550600, -46.648450), type: 'cliente'},  
+                        {position: new google.maps.LatLng(-23.543919, -46.666496), type: 'profissional'}
+                    ];
+
+                    infoWindow = new google.maps.InfoWindow;
+
+                    // Create markers.
+                    for (var i = 0; i < features.length; i++) {
+                        var marker = new google.maps.Marker({
+                        position: features[i].position,
+                        icon: icons[features[i].type].icon,
+                        map: map
+                        });
+                        google.maps.event.addListener( marker, 'click', function() {
+                            //infoWindow.setContent( this.info );
+                            //infoWindow.open( map, this );
+                            window.location.href = "pages/login.php";
+                        });            
+                    };
+
+                    var pos = {lat: -23.556583, lng: -46.658813};
+                        // Try HTML5 geolocation.
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function(position) {
+                        pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        infoWindow.setPosition(pos);
+                        infoWindow.setContent('Você está aqui');
+                        infoWindow.open(map);
+                        map.setCenter(pos);
+                        }, function() {
+                        handleLocationError(true, infoWindow, map.getCenter());
+                        });
+                    } else {
+                        // Browser doesn't support Geolocation
+                        handleLocationError(false, infoWindow, map.getCenter());
+                    }
+                }
+
+                function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent(browserHasGeolocation ?'Error: The Geolocation service failed.' :'Error: Your browser doesn\'t support geolocation.');
+                    infoWindow.open(map);                
+                }
+            </script>
+
+            <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaJ17V8D9xyAzg3_kmqeuuLqNrgZPdvL8&callback=initMap">
+            </script>
+
+        @else
         <main class="py-4">
             @yield('content')
         </main>
+        @endif
 
         <footer class="footer bg-footer container-fluid text-center">
             <p class="margin">APRER - Aplicativo Para Resoluções Residenciais  _  Todos os direitos reservados
