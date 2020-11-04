61aed64118b71ffcb2919f91284ef1f9d2c2c467
Xurlz <charlessena2010@bol.com.br>
2020-11-04 14:40:09 -0300

    Refatoração do mapa em pagina inicial

--- laravel/resources/views/mapa.blade.php
+++ laravel/resources/views/mapa.blade.php
@@ -1,19 +1,21 @@
-<section id="map">
-    <script>              
-        var map, infoWindow;
+<div id="map"></div>
+<script>              
+    var map, infoWindow;
 
-        function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
+    function initMap() {
+        console.log(document.getElementById('map'))
+        map = new google.maps.Map(document.getElementById('map'), 
+        {
             center: {lat: -23.556583, lng: -46.658813},
             zoom: 15
         });
 
         var icons = {
             profissional: {
-            icon: 'img/worker_30px.png'
+            icon: "{{asset('img/worker_30px.png')}}"
             },
             cliente: {
-            icon: 'img/hand_dollar.png'
+            icon: "{{asset('img/hand_dollar.png')}}"
             }
         };
 
@@ -63,15 +65,11 @@
             // Browser doesn't support Geolocation
             handleLocationError(false, infoWindow, map.getCenter());
         }
-        }
+    }
 
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?'Error: The Geolocation service failed.' :'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);                
-        }
-    </script>
-    <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaJ17V8D9xyAzg3_kmqeuuLqNrgZPdvL8&callback=initMap">
-    </script>
-</section>
+    }
+</script>
