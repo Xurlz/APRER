96650f7c04aa2f2888d9e43c1f3fcc2858be9bb2
Xurlz <charlessena2010@bol.com.br>
2020-11-05 18:20:25 -0300

    Inclusão de atributos de nome e valor para os campos do formulario

--- resources/views/teste-avaliacao.blade.php
+++ resources/views/teste-avaliacao.blade.php
@@ -5,8 +5,8 @@
         @csrf
         <div class="mb-3">
             <label class="form-label">Profissional</label>
-            <select class="form-control">
-                <option selected> Selecione um Profissional</option>
+            <select class="form-control" name="profissional">
+                <option value="" selected> Selecione um Profissional</option>
                 <option value="1"> Profissional a</option>
                 <option value="2"> Profissional b</option>
                 <option value="3"> Profissional c</option>
@@ -15,12 +15,12 @@
 
         <div class="mb-3">
             <label class="form-label">Nota</label>
-            <input type="number" class="form-control">
+            <input type="number" class="form-control" name = "nota">
         </div>
         
         <div class="mb-3">
             <label class="form-label">Avaliação</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name = "avaliacao">
         </div>
     
         <button type="submit" class="btn btn-danger">Enviar</button>
