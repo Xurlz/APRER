2957332aa71fa3a5f0557d71f030c657ac89bb25
Xurlz <charlessena2010@bol.com.br>
2020-11-06 13:48:07 -0300

    Inclusão de migration da tabela avaliacoes

---
+++ database/migrations/2020_11_06_163324_create_avaliacoes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvaliacoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avaliacoes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->from('users');
+            $table->text('texto')->nullable();
+            $table->integer('nota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avaliacoes');
+    }
+}
