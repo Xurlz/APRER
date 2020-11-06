c602fed77bcc044a82e72f8fbd3718c3ea267667
Xurlz <charlessena2010@bol.com.br>
2020-11-06 16:12:22 -0300

    Inclusão de model Avaliacao

---
+++ app/Models/Avaliacao.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Avaliacao extends Model
+{
+    use HasFactory;
+
+    protected $table = "avaliacoes";
+
+    protected $fillable = [
+        'user_id',
+        'texto',
+        'nota'
+    ];
+}
