c46b10d9f321c522940825db032b4de3cbfa8a1f
Xurlz <charlessena2010@bol.com.br>
2020-11-07 11:10:07 -0300

    Inclusão de update temporário

---
+++ database/update_tipo.sql
@@ -0,0 +1,5 @@
+-- SQLite
+UPDATE USERS SET type='admin' WHERE id = 1;
+UPDATE USERS SET type='cliente' WHERE id = 2;
+UPDATE USERS SET type='profissional' WHERE id = 3;
+UPDATE USERS SET type='profissional' WHERE id = 4;
